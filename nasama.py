abe5b3f0666240cc9bcab7efc61629fdd8bd64df
Thiago Ramos <thiagoramosdasilva16@gmail.com>
2026-05-11 04:29:57 -0300

    adiciona animações nas falas da assistente virtual

--- nasama.py
+++ nasama.py
@@ -1,16 +1,26 @@
 from gtts import gTTS
-from playsound import playsound
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame
 import speech_recognition as sr
 import sys
-import os
-import funcoes_so , funcoes_email
+import teste_os
+import teste_email
+import threading
+import interface
+import time
 
 
 
 def cria_audio(audio, mensagem):
     tts = gTTS(mensagem, lang = 'pt-br')
     tts.save(audio)
-    playsound(audio)
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio)
+    pygame.mixer.music.play() 
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)
+    pygame.mixer.quit() 
     os.remove(audio)
     
 
@@ -20,36 +30,71 @@ def monitora_audio(executar_comando=True):
         recon.adjust_for_ambient_noise(source, duration=1)
         while True:
             print("Diga alguma coisa")
-            audio = recon.listen(source, phrase_time_limit=10)
+            audio = recon.listen(source, timeout=10, phrase_time_limit=10)
             try:
                 mensagem = recon.recognize_google(audio, language= 'pt-br')
                 mensagem = mensagem.lower()
                 print("Você disse: ", mensagem)
+                interface.adiciona_historico("Você", mensagem)
             
                 if executar_comando:
                     executa_comandos(mensagem)
                 break
             except sr.WaitTimeoutError:
-                cria_audio("erro.mp3", "Tempo esgotado. Tente novamente.")
+                interface.inicia_animacao()
+                cria_audio("erro.mp3", "Não ouvi nada. Pode repetir?.")
+                interface.para_animacao()
             except sr.UnknownValueError:
+                interface.inicia_animacao()
                 cria_audio("erro.mp3", "Não consegui entender. Tente novamente.")
+                interface.para_animacao()
             except sr.RequestError:
+                interface.inicia_animacao()
                 cria_audio("erro.mp3", "Erro ao conectar ao serviço de reconhecimento.")
+                interface.para_animacao()
         return mensagem 
     
 
 def executa_comandos(acao):
     if 'fechar assistente' in acao:
+        interface.janela.after(0, interface.janela.destroy)
         sys.exit()
     elif 'horas' in acao:
-        cria_audio('mensagem.mp3', funcoes_so.verifica_hora())
+        hora = teste_os.verifica_hora()
+        interface.atualiza_status("falando")
+        interface.inicia_animacao()
+        interface.adiciona_historico("Nasama", hora)
+        cria_audio('mensagem.mp3', hora)
+        interface.para_animacao() 
+        interface.atualiza_status("ouvindo")
+        
     elif 'enviar email' in acao or 'enviar e-mail' in acao:
-        status_email = funcoes_email.enviar_email(cria_audio, monitora_audio)
+        interface.atualiza_status("ouvindo")
+        status_email = teste_email.enviar_email(cria_audio,monitora_audio)
+        interface.atualiza_status("falando")
+        interface.inicia_animacao()
+        interface.adiciona_historico("Nasama", status_email)
         cria_audio('mensagem.mp3', status_email)
+        interface.para_animacao()
+        interface.atualiza_status("ouvindo")
         
 
 def main():
+    thread_nasama = threading.Thread(target=inicia_nasama, daemon=True)
+    thread_nasama.start()
+    time.sleep(0.5) 
+    interface.janela.mainloop()
+    
+
+def inicia_nasama():
+    print("inicia_nasama rodando!")
+    time.sleep(2)
+    interface.inicia_animacao()
     cria_audio("wellcome.mp3", "Olá, sou a Nasama. Em que posso lhe ajudar?")
-    while True:
-        monitora_audio()    
+    interface.para_animacao()
+    interface.adiciona_historico("Nasama", "Olá, sou a Nasama. Em que posso lhe ajudar?")
+    while True:                              
+        interface.atualiza_status("ouvindo")
+        monitora_audio()
+
 main()
