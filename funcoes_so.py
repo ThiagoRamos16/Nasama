33935b7963d996eb25a97475ebd976bbe4397cbe
Thiago Ramos <thiagoramosdasilva16@gmail.com>
2026-05-13 18:52:33 -0300

    feat: adiciona funções de desligar e cancelar desligamento

--- funcoes_so.py
+++ funcoes_so.py
@@ -5,3 +5,12 @@ def verifica_hora():
     hora = datetime.now().strftime("%H:%M")
     frase = f"Agora são {hora}"
     return frase
+
+def desliga_computador_uma_hora():
+    os.system('shutdown /s /t 3600')
+    
+def desliga_computador_meia_hora():
+    os.system('shutdown /s /t 1800')
+    
+def cancela_desligamento():
+    os.system('shutdown /a')
